37e5f3557738e6f81c1d1fe2c0bad66f155056b8
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-09-16 16:14:42 -0300

    feat: add openapi documentation

--- src/main/java/br/senac/go/resource/PessoaResource.java
+++ src/main/java/br/senac/go/resource/PessoaResource.java
@@ -2,34 +2,46 @@ package br.senac.go.resource;
 
 import br.senac.go.interfaces.IResource;
 import br.senac.go.model.Pessoa;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController("api/pessoa")
+@Tag(name = "pessoa", description = "documentação da classe pessoa")
 public class PessoaResource implements IResource<Pessoa, Integer> {
     @Override
+    @PostMapping
     public Pessoa create(Pessoa entity) {
         return null;
     }
 
     @Override
+    @GetMapping
     public Pessoa read(Pessoa entity) {
         return null;
     }
 
     @Override
-    public Pessoa readById(Integer id) {
+    @GetMapping("/id")
+    public Pessoa readById(@PathVariable("id")  Integer id) {
         return null;
     }
 
     @Override
+    @PutMapping
     public Pessoa update(Pessoa entity) {
         return null;
     }
 
     @Override
-    public void delete(Integer id) {
+    @DeleteMapping("/id")
+    public void delete(@PathVariable("id") Integer id) {
 
     }
 
     @Override
+    @DeleteMapping
     public void deleteByEntity(Pessoa entity) {
 
     }
