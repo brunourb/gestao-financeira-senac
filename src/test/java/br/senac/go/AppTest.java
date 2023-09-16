24bf7a8df089653bf4bf03c1573c15e8f0205624
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-09-16 09:37:07 -0300

    chore(senac): setup inicial do projeto

---
+++ src/test/java/br/senac/go/AppTest.java
@@ -0,0 +1,18 @@
+package br.senac.go;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * Unit test for simple App.
+
+ */
+@SpringBootTest
+public class AppTest {
+    @Test
+    void contextLoads() {
+    }
+}
